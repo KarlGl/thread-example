b0ad1480af170e36d5ccc5300418523d75507409
Karl Glaser <kg@papercloud.com.au>
2014-03-17 01:33:22 +1100

    a

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 threads = 10
+var = '(none)'
 
 threads.times.map do |i|
 	Thread.new {
 		sleep rand
-		puts "Thread #{i} has finished."
+		puts "Thread #{i} has finished after #{var}."
+		var = i
 	}
 end
 
